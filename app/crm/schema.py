a6c87be95c8e7e5c04c5f26ecaf4be69bbdb509c
Diman4a12 <Diman4a65@gmail.com>
2022-08-18 15:56:02 +0500

    2day

---
+++ app/crm/schema.py
@@ -0,0 +1,24 @@
+from marshmallow import Schema, fields
+
+from app.web.schema import OkResponseSchema
+
+class UserAddSchema(Schema):
+    mail = fields.Str(required=True)
+
+class UserSchema(UserAddSchema):
+    id = fields.UUID(required=True)
+    
+class UserGetRequestSchema(Schema):
+    id = fields.UUID(required=True)
+    
+class UserGetSchema(Schema):
+    user = fields.Nested(UserSchema)
+    
+class UserGetResponseSchema(OkResponseSchema):
+    data = fields.Nested(UserGetSchema)
+    
+class ListUsersSchema(Schema):
+    users = fields.Nested(UserSchema, many=True)
+    
+class ListUsersResponseSchema(OkResponseSchema):
+    data = fields.Nested(ListUsersSchema)
